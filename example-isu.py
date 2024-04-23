43f6c3c6209faaed3feaae5d88377e838a428033
Alex Berman <alexander.berman@gu.se>
2024-04-23 15:59:46 +0200

    example-isu.py: Extend game of fetch

--- example-isu.py
+++ example-isu.py
@@ -17,10 +17,27 @@ print(
 
 # Game of fetch, corresponding to Cooper (2023, p.55) (only first function)
 pick_up = Pred('pick_up', [Ind, Ind])
+attract_attention = Pred('attract_attention', [Ind, Ind])
 f1 = Fun('r',
          RecType({'agenda': SingletonType(ListType(Ty), [])}),
          RecType({'agenda': SingletonType(ListType(Ty), [
              {'e': PType(pick_up, ['a', 'c'])}
          ])}))
-state_0 = RecType({'agenda': SingletonType(ListType(Ty), [])})
-print(show(f1.app(state_0)))
+f2 = Fun('r',
+         RecType({'agenda': SingletonType(ListType(Ty), [
+             {'e': PType(pick_up, ['a', 'c'])}
+         ])}),
+         Fun('e',
+             RecType({'e': PType(pick_up, ['a', 'c'])}),
+             RecType({'agenda': SingletonType(ListType(Ty), [
+                 {'e': PType(attract_attention, ['a', 'b'])}
+             ])})))
+state_0 = RecType({'agenda': SingletonType(ListType(Ty), [])}) # Initial state
+state_1 = f1.app(state_0) # State after applying f1
+state_2 = f2.app(state_1).app(RecType({'e': PType(pick_up, ['a', 'c'])})) # State after applying f2 and pick-up event
+print(show(state_1))
+print(show(state_2))
+
+# TODO:
+# - add more states?
+# - replace manual invocation of state transitions above with action rule(s)
