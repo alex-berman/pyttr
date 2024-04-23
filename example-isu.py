e0d3e2e6528b77372589b4122517bbb6ded10c5f
Alex Berman <alexander.berman@gu.se>
2024-04-23 14:55:11 +0200

    example-isu.py: Add game of fetch

--- example-isu.py
+++ example-isu.py
@@ -1,4 +1,4 @@
-from ttrtypes import BType, PType, Pred, ListType, SingletonType, RecType, Ty, Re
+from ttrtypes import BType, PType, Pred, ListType, SingletonType, RecType, Ty, Re, Fun
 from utils import show
 
 
@@ -13,3 +13,14 @@ print(
         T1.amerge(T2)
         )
         )
+
+
+# Game of fetch, corresponding to Cooper (2023, p.55) (only first function)
+pick_up = Pred('pick_up', [Ind, Ind])
+f1 = Fun('r',
+         RecType({'agenda': SingletonType(ListType(Ty), [])}),
+         RecType({'agenda': SingletonType(ListType(Ty), [
+             {'e': PType(pick_up, ['a', 'c'])}
+         ])}))
+state_0 = RecType({'agenda': SingletonType(ListType(Ty), [])})
+print(show(f1.app(state_0)))
