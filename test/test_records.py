d84166870dfd95bd2877a56a60efcb8fa1081818
Alex Berman <alexander.berman@gu.se>
2024-04-30 10:36:24 +0200

    Add unit tests for RecType.query

--- test/test_records.py
+++ test/test_records.py
@@ -1,4 +1,5 @@
 from records import Rec
+from ttrtypes import RecType, SingletonType, ListType, Ty, PType, Pred, BType
 from utils import show
 
 
@@ -7,3 +8,21 @@ class TestRecords(object):
         assert show(Rec({'a': {'b': 'c', 'd': 'e'}})) in [
             '{a = {b = c, d = e}}',
             '{a = {d = e, b = c}}']
+
+    def test_query_with_empty_value(self):
+        Ind = BType('Ind')
+        pick_up = Pred('pick_up', [Ind, Ind])
+        rec_type = RecType({'agenda': SingletonType(ListType(Ty), [])})
+        valid_arg = RecType({'agenda': SingletonType(ListType(Ty), [])}).create()
+        invalid_arg = RecType({'agenda': SingletonType(ListType(Ty), [{'e': PType(pick_up, ['a', 'c'])}])}).create()
+        assert rec_type.query(valid_arg)
+        assert not rec_type.query(invalid_arg)
+
+    def test_query_with_non_empty_value(self):
+        Ind = BType('Ind')
+        pick_up = Pred('pick_up', [Ind, Ind])
+        rec_type = RecType({'agenda': SingletonType(ListType(Ty), [{'e': PType(pick_up, ['a', 'c'])}])})
+        valid_arg = RecType({'agenda': SingletonType(ListType(Ty), [{'e': PType(pick_up, ['a', 'c'])}])}).create()
+        invalid_arg = RecType({'agenda': SingletonType(ListType(Ty), [])}).create()
+        #assert rec_type.query(valid_arg) # Should work?
+        assert not rec_type.query(invalid_arg)
